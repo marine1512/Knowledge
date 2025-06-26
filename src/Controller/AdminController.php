c4009b8c308003f1f0c88f90c3f7f5870b7735e5
Marine Mignot <119693729+marine1512@users.noreply.github.com>
2025-06-26 18:19:24 +0200

    AdminController.php

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserPurchaseRepository;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\UserPurchase;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * Class AdminController
@@ -27,4 +31,36 @@ class AdminController extends AbstractController
             'controller_name' => 'AdminController',
         ]);
     }
+
+    #[Route('/admin/achats', name: 'admin_achats')]
+    public function handlePurchases(UserPurchaseRepository $userPurchaseRepository, Request $request): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        
+        // Retrieve all purchases from the repository
+        $purchases = $userPurchaseRepository->findAll();
+
+        return $this->render('admin/achats/index.html.twig', [
+            'purchases' => $purchases
+        ]);
+    }
+
+    /**
+     * Deletes a specific purchase.
+     *
+     * @param UserPurchase $purchase
+     * @param ManagerRegistry $doctrine
+     * @return Response
+     */
+    #[Route('/admin/achats/supprimer/{id}', name: 'admin_delete_purchase')]
+public function deletePurchase(UserPurchase $purchase, ManagerRegistry $doctrine): Response
+{
+    $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+    $em = $doctrine->getManager();
+    $em->remove($purchase);
+    $em->flush();
+
+    return $this->redirectToRoute('admin_achats');
+}
 }
